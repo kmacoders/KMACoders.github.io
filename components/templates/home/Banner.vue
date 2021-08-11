efd0409606e06efc2fad7738ef448992d9b650d5
kmacoders <rio@rios-MacBook-Pro.local>
2021-08-11 23:20:48 +0700

    feat: add new banner

--- components/templates/home/Banner.vue
+++ components/templates/home/Banner.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="hero-body">
-    <div class="container has-text-centered">
-      <div class="columns is-vcentered">
-        <div class="column is-5">
-          <figure class="image is-5by3">
-            <img
-              class="main-image"
-              src="~assets/images/phungthaihung.jpeg"
-              alt="Description"
-            >
-          </figure>
-        </div>
-        <div class="column is-6 is-offset-1">
-          <h1 class="title is-2">
-            Superhero Scaffolding
-          </h1>
-          <h2 class="subtitle is-4">
-            Let this cover page describe a product or service.
-          </h2>
-          <br>
-          <p class="has-text-centered">
-            <a class="button is-medium is-info is-outlined">
-              Learn more
-            </a>
-          </p>
+  <section class="hero is-white">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns reverse-columns">
+          <div
+            class="column is-10-mobile is-4-tablet is-4-desktop is-4-widescreen is-4-fullhd  aos-init aos-animate"
+            data-aos="fade-down"
+          >
+            <h1 class="title titled is-1 mb-6">
+              Hi,<br>
+              I am <span style="color: rgb(255, 99, 3);">Rio</span>
+            </h1>
+            <h2 class=" subtitled subtitle has-text-grey is-4 has-text-weight-normal is-family-sans-serif">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+              nesciunt totam magni quas.
+            </h2>
+            <div class="buttons">
+              <button class="button is-primary">Resume</button>
+              <button class="button">Contact</button>
+            </div>
+          </div>
+          <div
+            data-aos="fade-right"
+            class="column is-10-mobile is-4-tablet is-4-desktop is-4-widescreen is-4-fullhd aos-animate">
+            <figure class="image is-square">
+              <img src="~assets/images/home/banner.svg">
+            </figure>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script>
 import { Vue, Component } from 'nuxt-property-decorator'
@@ -37,3 +40,9 @@ export default class Banner extends Vue {
 
 }
 </script>
+<style lang="scss" scoped>
+.columns {
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
