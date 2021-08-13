be13e6b3a4fa654b9ae5d0e80e43982f881b5dff
kmacoders <rio@rios-MacBook-Pro.local>
2021-08-13 21:19:37 +0700

    feat(icon): add tech icons

---
+++ components/atoms/icon/IconNuxt.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="256px"
+    height="189px"
+    viewBox="0 0 256 189"
+    version="1.1"
+    preserveAspectRatio="xMidYMid"
+  >
+    <g>
+      <polygon fill="#48B884" points="109.7766 0.0009 -0.0004 188.2259 219.5526 188.2259" />
+      <polygon fill="#3F7F70" points="72.8361 188.226 255.9991 188.226 164.4181 31.198" />
+      <polygon fill="#36495D" points="72.8361 188.226 219.5521 188.226 146.1941 62.444" />
+    </g>
+  </svg>
+</template>
