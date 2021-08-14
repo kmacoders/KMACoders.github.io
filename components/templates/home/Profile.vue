99da5c0b4953f64a688c8ed8d88ea3efa216a3ae
kmacoders <hungkids3003@gmail.com>
2021-08-14 17:37:58 +0700

    refactor: separate svg to atom

--- components/templates/home/Profile.vue
+++ components/templates/home/Profile.vue
@@ -5,20 +5,23 @@
       <div class="columns">
         <div class="column is-12-mobile is-6-tablet">
           <div class="profile-avatar">
-            <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" id="blobSvg">
-              <defs>
-              <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-              <stop offset="0%" style="stop-color: rgb(255, 99, 3);"></stop>
-              <stop offset="100%" style="stop-color: rgb(255, 195, 113);"></stop>
-              </linearGradient>
-              </defs>
-              <path id="blob" d="M471,303.5Q455,357,419.5,400.5Q384,444,330,455Q276,466,220.5,469.5Q165,473,129,431Q93,389,66.5,345.5Q40,302,35.5,249Q31,196,60,151.5Q89,107,127.5,67.5Q166,28,220.5,36Q275,44,320,63.5Q365,83,405.5,115Q446,147,466.5,198.5Q487,250,471,303.5Z" fill="url(#gradient)"></path>
-            </svg>
+            <BlobProfile />
             <img src="~assets/images/home/phungthaihung.png">
           </div>
         </div>
         <div class="column is-12-mobile is-6-tablet">
-          Phung Thai Hung
+          <p class="has-text-primary">My info</p>
+          <h3 class="title is-3">About Me</h3>
+          <div class="profile-info">
+            <div class="profile-info__item">
+              <div class="profile-info__icon">
+                Name
+              </div>
+              <div class="profile-info__detail">
+                Phung Thai Hung
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,9 +30,11 @@
 
 <script>
 import { Vue, Component } from 'nuxt-property-decorator'
+import BlobProfile from '~/components/atoms/illustrator/BlobProfile.vue'
 
 @Component({
   components: {
+    BlobProfile
   }
 })
 export default class Profile extends Vue {
