b1692e3843de8365c74a6bf93829f60e60d76ba3
kmacoders <kmacoders@users.noreply.github.com>
2021-08-26 16:32:48 +0000

    deploy: c53794d7863980a5503485ecb61d2bee1a097d02

---
+++ _nuxt/static/1629995546/tags/Optimize/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629995546",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002FOptimize",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"873d31e0"}}}}(null));
