ae3466c1fbfeb54de56b39b149746ef97a194025
kmacoders <kmacoders@users.noreply.github.com>
2021-08-26 16:16:10 +0000

    deploy: 4bc7b4648c942f195f22b642bc91e342c0c3cd3a

--- _nuxt/static/1629993607/blog/custom-mot-switch-giong-tren-ios/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629993607",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fcustom-mot-switch-giong-tren-ios",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"69a36960"}}}}(null));
