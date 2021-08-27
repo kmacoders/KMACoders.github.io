a348809a87877ab0a468fdfbcb3f5de9606aa5de
kmacoders <kmacoders@users.noreply.github.com>
2021-08-27 10:23:24 +0000

    deploy: 2d1f1434bea3ac90662e3745a3f319f5d7112dbd

---
+++ _nuxt/static/1630059795/sign-in/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630059795",layout:"basic",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"40c2de62"}}}}("\u002Fsign-in"));
