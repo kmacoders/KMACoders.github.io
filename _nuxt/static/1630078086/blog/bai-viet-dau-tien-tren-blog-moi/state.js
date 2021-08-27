e0c7e80eb52943ce6bb651740ec017005924c2a4
kmacoders <kmacoders@users.noreply.github.com>
2021-08-27 15:28:15 +0000

    deploy: aee21c7fef5b10c88cb7d89818fa37def2b68910

---
+++ _nuxt/static/1630078086/blog/bai-viet-dau-tien-tren-blog-moi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630078086",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fbai-viet-dau-tien-tren-blog-moi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"674ed725"}}}}(null));
