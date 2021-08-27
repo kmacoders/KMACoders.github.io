b87ba723c962f99e635b65105055855cebd03a21
kmacoders <kmacoders@users.noreply.github.com>
2021-08-27 14:20:49 +0000

    deploy: 53fe3d2e2a2b22588a8216a30047567008451e4e

--- _nuxt/static/1630059795/tags/stories/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630059795",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fstories",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40c2de62"}}}}(null));
