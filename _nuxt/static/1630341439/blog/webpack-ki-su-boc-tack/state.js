7d8ca915f53146383c865bdefbf54183652397f4
kmacoders <kmacoders@users.noreply.github.com>
2021-08-30 16:43:26 +0000

    deploy: 9e94577c1394077c2f0005febd4387a5fb4cf140

--- _nuxt/static/1630341439/blog/webpack-ki-su-boc-tack/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630341439",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwebpack-ki-su-boc-tack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"442e02cf"}}}}(null));
