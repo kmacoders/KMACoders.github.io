a279e0ca43c946eb8fb35d84d6b8a65afd3a493c
kmacoders <kmacoders@users.noreply.github.com>
2021-09-02 18:22:23 +0000

    deploy: 68dc1437ae96d59ebaf81013719d23f152390dfb

--- _nuxt/static/1630580017/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630580017",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{baseUrl:"https:\u002F\u002Fkmacoders.github.io",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"357012df"}}}}(null));
