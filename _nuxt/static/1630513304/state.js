ab659ccd6c8e1a603a7f8bdd01f5bdb47c13399d
kmacoders <kmacoders@users.noreply.github.com>
2021-09-02 10:36:20 +0000

    deploy: e7b11cc5744a181067263e18195590c4c77a4ec2

--- _nuxt/static/1630513304/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630513304",layout:"default",error:a,serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Fkmacoders.github.io",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4dc807a0"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1630578967/tags/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630578967",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags",config:{baseUrl:"https:\u002F\u002Fkmacoders.github.io",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"af250de7"}}}}(null));
