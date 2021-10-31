33ceebaa855717bd8ffd2f2461506aa4a814e646
kmacoders <kmacoders@users.noreply.github.com>
2021-10-31 12:37:47 +0000

    deploy: 120f33c211df3ac46600cb440be930d075ee2d8e

---
+++ _nuxt/static/1635683856/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635683856",layout:"default",error:a,serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Fkmacoders.github.io",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a1402eb"}}}}(null,"\u002F"));
